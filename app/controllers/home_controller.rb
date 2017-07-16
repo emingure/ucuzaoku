542f8db0dd52bddd971e55858cc2012fa8cfe7ae
emin gure <muhammed.gure@boun.edu.tr>
2017-07-16 17:53:32 +0300

    Add author, publisher,image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,8 +31,19 @@ class HomeController < ApplicationController
         link.css('div.name a').each do |url|
           info['url'] = url['href']
         end
+        link.css('div.image img').each do |img_url|
+          info['img_url'] = img_url['src']
+        end
         link.css('div.name a span').each do |title|
           info['title'] = title.content
+        end
+
+        link.css('div.author span a span').each do |author|
+          info['author'] = author.content
+        end
+
+        link.css('div.publisher span a span').each do |publisher|
+          info['publisher'] = publisher.content
         end
 
         link.css('div.price div.price-new span.value').each do |price|
@@ -56,8 +67,17 @@ class HomeController < ApplicationController
         link.xpath('div[1]/div[1]/a[1]').each do |url|
           info['url'] = 'http://www.dr.com.tr'+url['href']
         end
+        link.xpath('figure/a/img').each do |img_url|
+          info['img_url'] = img_url['src']
+        end
         link.xpath('div[1]/div[1]/a[1]/h3').each do |title|
           info['title'] = title.content
+        end
+        link.xpath('div[1]/div[1]/a[2]').each do |author|
+          info['author'] = author.content
+        end
+        link.xpath('div[1]/div[1]/a[3]').each do |publisher|
+          info['publisher'] = publisher.content
         end
 
         link.xpath('div[1]/div[2]/span[2]').each do |price|
@@ -81,8 +101,17 @@ class HomeController < ApplicationController
         link.xpath('div[1]/div[1]/a[1]').each do |url|
           info['url'] = 'http://www.idefix.com'+url['href']
         end
+        link.xpath('figure/a/img').each do |img_url|
+          info['img_url'] = img_url['src']
+        end
         link.xpath('div[1]/div[1]/a[1]/h3').each do |title|
           info['title'] = title.content
+        end
+        link.xpath('div[1]/div[1]/a[2]').each do |author|
+          info['author'] = author.content
+        end
+        link.xpath('div[1]/div[1]/a[3]').each do |publisher|
+          info['publisher'] = publisher.content
         end
 
         link.xpath('div[1]/div[2]/span[2]').each do |price|
@@ -109,6 +138,15 @@ class HomeController < ApplicationController
           info['url'] = 'http://www.babil.com'+url['href']
           info['title'] = url.content
         end
+        link.xpath('div[1]/div/a/img').each do |img_url|
+          info['img_url'] = img_url['src']
+        end
+        link.xpath('div[2]/div/h3/a').each do |author|
+          info['author'] = author.content
+        end
+        link.xpath('div[2]/div/h4/a').each do |publisher|
+          info['publisher'] = publisher.content
+        end
 
         link.xpath('div[2]/div/div[2]/span[2]').each do |price|
           info['price'] = price.content
@@ -129,7 +167,10 @@ class HomeController < ApplicationController
       search_result.css('li.urunorta').each do |link|
         info = {}
         info['url'] = 'http://pandora.com.tr' + link.css('.imgcont a')[0]['href']
+        info['img_url'] = 'http://pandora.com.tr' + link.css('.imgcont a img')[0]['src']
         info['title'] = link.css('.kt').text
+        info['author'] = link.css('.yz').text
+        info['publisher'] = link.css('.yy').text
         info['price'] = link.css('.se .fyt strong').text
 
         if info['price'].present?
@@ -145,7 +186,10 @@ class HomeController < ApplicationController
       search_result.css('ul.product-list li div.product-list-right-top').each do |link|
         info = {}
         info['url'] = link.xpath('div[1]/h4/a')[0]['href']
+        info['img_url'] = 'http://www.nadirkitap.com/' + link.parent.parent.xpath('div[1]/a/img')[0]['src']
         info['title'] = link.xpath('div[1]/h4/a/span').text
+        info['author'] = link.xpath('div[1]/p').text
+        info['publisher'] = link.xpath('div[1]/ul/li[2]/span').text
         info['price'] = link.xpath('div[2]/div[3]').text
         if info['price'].present?
           @content << info
